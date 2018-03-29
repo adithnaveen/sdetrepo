5b30ad958e1efa6566b71e728bece000953b1dba
Naveen <adith.naveen@gmail.com>
2018-03-29 08:09:36 +0530

    new line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ package com.sdet;
 public class HelloWorld{
 	public static void main(String args[]){
 		
+		System.out.println("This is new line " );
+		
 		for(int i=0; i<10; i++){
 			System.out.println("i value is " + i);
 		}
