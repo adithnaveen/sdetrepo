5428d7cddc0d0ec198d8cf56a208be6f7efbadf0
Naveen <adith.naveen@gmail.com>
2018-03-29 07:38:36 +0530

    first application

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sdet; 
+
+
+public class HelloWorld{
+	public static void main(String args[]){
+		
+		for(int i=0; i<10; i++){
+			System.out.println("i value is " + i);
+		}
+		
+		System.out.println("Program terminating ");
+	}
+}
